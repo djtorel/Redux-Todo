ad1d96996885079df054bbab9019688b3afdbc81
Dominic Torruellas <dom@domtorr.com>
2019-06-22 10:13:58 -0700

    hooked up react-redux store, provider and set up basic component

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import TodoCard from './components/TodoCard';
+
 function App() {
   return (
     <div className="App">
-      <div />
+      <TodoCard />
     </div>
   );
 }
---
+++ todo/src/components/TodoCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const TodoCard = props => {
+  return (
+    <div>
+      <div />
+    </div>
+  );
+};
+
+const mapStateToProps = () => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  {}
+)(TodoCard);
