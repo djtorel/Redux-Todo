136cbc357884d9c0f5ca2b3947210ee9170cd359
Dominic Torruellas <dom@domtorr.com>
2019-06-22 14:20:49 -0700

    tweaked button style for deleting todos

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -32,7 +32,7 @@ const todoCompletedCSS = css`
 
 const button = css`
   border: 0;
-  background-color: #9a5a5a;
+  background-color: #ba5a5a;
   border-radius: 0 3px 3px 0;
   cursor: pointer;
 `;
