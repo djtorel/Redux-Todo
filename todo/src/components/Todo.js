271b695d21f68540fe00cc439988c835c01f446c
Dominic Torruellas <dom@domtorr.com>
2019-06-22 12:54:40 -0700

    tweaked styles

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -20,10 +20,11 @@ const todoCSS = css`
 
 const todoCompletedCSS = css`
   padding: 5px;
-  color: #4a4a4a;
+  color: #3a3a3a;
   background-color: #5a5a5a;
   border-radius: 3px;
   text-align: center;
+  text-decoration: line-through;
 `;
 
 const Todo = ({ todo: { value, id, completed }, toggleTodo }) => {
