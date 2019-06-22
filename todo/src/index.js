2b8475acdc76291e9c936a5284885190be591ddb
Dominic Torruellas <dom@domtorr.com>
2019-06-22 13:43:17 -0700

    Stretch - Using local storage to persist state

--- todo/src/index.js
+++ todo/src/index.js
@@ -7,10 +7,18 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 
+const persistedState =
+  JSON.parse(localStorage.getItem('reduxState')) || undefined;
+
 const store = createStore(
   reducers,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()));
+});
 
 ReactDOM.render(
   <Provider store={store}>
