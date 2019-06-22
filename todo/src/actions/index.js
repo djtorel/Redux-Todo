f1c45b264c8bb15b827100f0dc42e2a8b67f5117
Dominic Torruellas <dom@domtorr.com>
2019-06-22 11:42:50 -0700

    added toggle-todo action

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,4 +1,5 @@
-import { ADD_TODO } from './action-items';
+import { ADD_TODO, TOGGLE_TODO } from './action-items';
 import addTodo from './add-todo';
+import toggleTodo from './toggle-todo';
 
-export { ADD_TODO, addTodo };
+export { ADD_TODO, TOGGLE_TODO, addTodo, toggleTodo };
